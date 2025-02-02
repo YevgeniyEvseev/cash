926babdeea8d8b0eba8d0b917c25f36619b611d0
dotharer <dotharer@student.21-school.ru>
2025-02-02 21:27:59 +0700

    hash

--- cash.h
+++ cash.h
@@ -5,6 +5,14 @@
 #define POLINOM 23
 #define SIZE_INT 32
 
+struct page {
+  int index;
+  char data[60];
+};
+
+void slow_get_page(int n, struct page *p);
+void get_page(int id, struct page *p);
+
 int insert_hash(char *str);
 int search_hash(char *str);
 int hash_str(char *str, unsigned a, unsigned b);
